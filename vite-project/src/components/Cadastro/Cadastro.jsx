be63c62dd9c15b440f301eadabc26766a73bf45a
daianefsilvadai <daianefsilvadai@gmail.com>
2024-05-29 11:36:01 -0300

    Criação do novo componente de cadastro, inclusão de imagem.

---
+++ vite-project/src/components/Cadastro/Cadastro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Medicine from "../../assets/medicine.svg";
+
+const Cadastro = () => {
+  return (
+    <section>
+      <h1>Cadastro</h1>
+      <img src={Medicine} alt="" />
+    </section>
+  );
+};
+
+export default Cadastro;
